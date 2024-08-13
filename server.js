c977bb57cdd364bb322046e6636d217045c54548
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-08-13 23:22:17 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { parse } = require("path");
+let user;
+fs.readFileSync("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 //1: Kirish kodlari
 app.use(express.static("public"));
@@ -12,15 +22,19 @@ app.use(express.urlencoded({ extended: true }));
 //2: Session code
 
 //3: Views code
-app.set("views", "views");
+app.set("views", "views"); //
 app.set("view engine", "ejs");
 
 //4: Routing Code
 
 app.post("/create-item", (req, res) => {
   console.log(req.body);
-  res.json({ "test": "success" });
+  res.json({ test: "success" });
   //TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
