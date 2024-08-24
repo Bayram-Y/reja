1931bcaf885184f9721caccf4ebfb3af392c3903
Bayram-Y <bayramyaxiyaev7@gmail.com>
2024-08-24 13:37:12 +0900

    feat: completed MIT- C task

--- train.js
+++ train.js
@@ -167,3 +167,61 @@ MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 // }
 
 // countDigits("ad2a54y79wet0sfgb9");
+
+/*
+MITASK-C 
+
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+*/
+
+const moment = require("moment");
+
+class Shop {
+  //state
+  non;
+  lagmon;
+  cola;
+  //constructor
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+  //method
+  qoldiq() {
+    console.log(
+      ` Hozir ${moment().format("hh:mm")} da ${this.non} non , ${
+        this.lagmon
+      } lagmon va ${this.cola} cola mavjud !`
+    );
+  }
+
+  sotish(narsa, soni) {
+    if (narsa === "non" || narsa === "lagmon" || narsa === "cola") {
+      console.log(
+        `Hozir ${moment().format("hh:mm")} da ${narsa} ${soni} ta sotildi!`
+      );
+      this[narsa] -= soni;
+    }
+  }
+
+  qabul(tovar, son) {
+    if (tovar === "non" || tovar === "lagmon" || tovar === "cola") {
+      console.log(
+        `Hozir ${moment().format("hh:mm")} da ${tovar} ${son} ta sotildi!`
+      );
+      this[tovar] += son;
+    }
+  }
+}
+
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();
+console.log("=========================================================");
+
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+console.log("=========================================================");
+shop.qoldiq();
